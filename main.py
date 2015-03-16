35122920d5bde1af64534712d169d76ac7950c79
mihai <myhay.360@gmail.com>
2015-03-16 14:03:19 +0100

    First Version.

--- main.py
+++ main.py
@@ -1,42 +1,76 @@
-__author__ = 'myhay'
+#!/usr/bin/python
+from __future__ import print_function
+import sys
+__author__ = 'mihai'
 
 def readFile(fname):
-    binariMatrix = []
+    """
+
+    :rtype : (BinaryMatrix,OriginalMatrix)
+    """
+    binaryMatrix = []
+    originalMatrix = []
     with open(fname) as f:
-        line = []
+        binaryLine = []
+        originalLine = []
         while True:
             c = f.read(1)
             if not c:
                 break
             elif c == ".":
-                line.append(1)
+                binaryLine.append(1)
+                originalLine.append(".")
             elif c == "o":
-                line.append(0)
+                binaryLine.append(0)
+                originalLine.append("o")
             elif c == "\n":
-                binariMatrix.append(line)
-                line = []
-    return binariMatrix
+                binaryMatrix.append(binaryLine)
+                originalMatrix.append(originalLine)
+                binaryLine = []
+                originalLine = []
+    return (binaryMatrix, originalMatrix)
 
-def max_size(mat, ZERO=0):
+
+def printMaxSubSquare(mat, orMat):
     nrows = len(mat)
     ncols = 0
     if mat:
         ncols = len(mat[0])
     if not (nrows and ncols):
         return 0
-    counts = [[0]*ncols for _ in xrange(nrows)]
-    for i in reversed(xrange(nrows)):     # for each row
-        assert len(mat[i]) == ncols # matrix must be rectangular
-        for j in reversed(xrange(ncols)): # for each element in the row
-            if mat[i][j] != ZERO:
-                counts[i][j] = (1 + min(
-                    counts[i][j+1],  # east
-                    counts[i+1][j],  # south
-                    counts[i+1][j+1] # south-east
-                    )) if i < (nrows - 1) and j < (ncols - 1) else 1 # edges
-    print(counts)
-    return max(c for rows in counts for c in rows)
+    subMat = [[0] * ncols for _ in xrange(nrows)]
 
-matrix = readFile("example_file.txt")
+    ## Find the maximum entry, and indexes of maximum entry in subMat[][] ##
+    for i in range(nrows):
+        for j in range(ncols):
+            if mat[i][j] == 1:
+                subMat[i][j] = min(subMat[i][j - 1], subMat[i - 1][j], subMat[i - 1][j - 1]) + 1
+            else:
+                subMat[i][j] = 0;
 
-print max_size(matrix)
+    max_of_SubMax, max_i, max_j = subMat[0][0], 0, 0
+    for i in range(nrows):
+        for j in range(ncols):
+            if max_of_SubMax < subMat[i][j]:
+                max_of_SubMax = subMat[i][j]
+                max_i = i
+                max_j = j
+
+    #print(max_of_SubMax, max_i, max_j)
+
+    for i in range(max_i, max_i - max_of_SubMax, -1):
+        for j in range(max_j, max_j - max_of_SubMax, -1):
+            orMat[i][j] = "x"
+
+    for i in orMat:
+        for j in i:
+            print(j,end ="")
+        print()
+
+if __name__ == "__main__":
+    argument = len(sys.argv)
+    if argument != 2:
+        print("Help: console#> main.py fileToRead")
+    else:
+        binaryMatrix, orignalMatrix = readFile(sys.argv[1])
+        printMaxSubSquare(binaryMatrix, orignalMatrix)
